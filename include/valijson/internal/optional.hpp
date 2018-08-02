c2f22fddf599d04dc33fcd7ed257c698a05345d9
Tristan Penman <tristan@tristanpenman.com>
2018-08-02 11:57:08 +1000

    Minor change to internal/optional.hp so that __has_include is used only if __cplusplus >= 201703

--- include/valijson/internal/optional.hpp
+++ include/valijson/internal/optional.hpp
@@ -2,10 +2,14 @@
 #ifndef __VALIJSON_OPTIONAL_HPP
 #define __VALIJSON_OPTIONAL_HPP
 
-// This should be removed once C++17 is widely available
-#if __has_include(<optional>) && __cplusplus >= 201703
-#  include <optional>
+#if __cplusplus >= 201703
+// Visual C++ only supports __has_include in versions 14.12 and greater
+#  if !defined(_MSC_VER) || _MSC_VER >= 1912
+#    if __has_include(<optional>)
+#      include <optional>
 namespace opt = std;
+#    endif
+#  endif
 #else
 #  include <compat/optional.hpp>
 namespace opt = std::experimental;
